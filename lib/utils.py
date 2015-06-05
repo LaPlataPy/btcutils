b32ea086b29404b314045c53a7a3476f4d5c5fb9
ilmesi <nachoguason@gmail.com>
2015-06-05 10:17:43 -0300

    cleaning utils

--- lib/utils.py
+++ lib/utils.py
@@ -1,28 +1,16 @@
 
 
-def egcd(a, b):
-    """
-    Extended Euclidean Algorithm
-    http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-    http://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm
-    """
-    x,y, u,v = 0,1, 1,0
-    while a != 0:
-        q, r = b//a, b%a
-        m, n = x-u*q, y-v*q
-        b,a, x,y, u,v = a,r, u,v, m,n
-    gcd = b
-    return gcd, x, y
+def hexlify(num):
+    pass
 
-def modular_inverse_with_egcd(a, m):
-    # TODO: WIP not working
-    gcd, x, y = egcd(a, m)
-    return x % m
 
 def modular_inverse(a, n):
     """
     Used for division in elliptic curves. Very important in RSA/ECDSA algorithms.
-    It uses EGCD 
+    It uses EGCD.
+    Extended Euclidean Algorithm:
+        - http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+        - http://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm
     """
     lm, hm = 1,0
     low, high = a%n,n
